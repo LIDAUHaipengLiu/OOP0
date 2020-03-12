3976eda2a4101eb348a32ca47b4eb92a615bfd60
LIDAUHaipengLiu <55245401+LIDAUHaipengLiu@users.noreply.github.com>
2020-03-11 18:00:18 -0700

    Add files via upload

---
+++ 0/Class3.1/Class3.1/Backup/Class3.1/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class3._
+{
+    public class Student
+    {
+        
+        /// <summary>
+        /// 学号
+        /// </summary>
+
+        public int _stuNum;
+        /// <summary>
+        /// 姓名
+        /// </summary>
+                                                        
+        public string _stuName;
+        /// <summary>
+        /// 性别
+        /// </summary>
+                                                        	                                         
+        public char _stuSex;
+        /// <summary>
+        /// 年龄
+        /// </summary>
+                                                        
+        public int _stuAge;
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <returns></returns>
+                                                       
+        public string GetStuInfo()
+        {
+            string _info = "学号:" + _stuNum + "\t 姓名:" + _stuName + "\t 性别:" + _stuSex + "\t 年龄:" + _stuAge;
+            return _info;
+        }
+
+    }
+}
